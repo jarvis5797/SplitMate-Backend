5c1220565881c90a7ead320becc58c7ef15f06c6
Shivam Dubey <dubeyshivam104@gmail.com>
2026-01-30 19:42:23 +0530

    setup the project

---
+++ src/main/java/com/example/spiltmate/backend/SplitmateBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.spiltmate.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitmateBackendApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
